9e22cc2191a32f7d5fc7fc5144a784445f5868f7
Megan Guercio <mmg16d@my.fsu.edu>
2019-10-04 19:12:16 -0400

    Committed Jet.h

---
+++ Jet.h
@@ -0,0 +1,26 @@
+//Jet.h
+//Megan Guercio
+
+#ifndef DRIVINGSIMULATOR_CAR_H
+#define DRIVINGSIMULATOR_CAR_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle{
+private:
+  int numberOfEngines;
+
+public:
+
+  Jet();
+  explicit Jet(string brand, string model, string fuelType, int engNum);
+
+  virtual ~Jet();
+  int getEngineSize();
+  void setEngineSize();
+  virtual double mileageEstimate(double time);
+  virtual string toString();
+
+};
+
+#endif //DRIVINGSIMULATOR_CAR_H
